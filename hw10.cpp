d6e3a4ad9c8cd9df83ef5c3c48fe9c45639a6555
dja158 <djanderson158@gmail.com>
2015-07-18 23:06:24 -0600

    Create hw10.cpp

---
+++ hw10.cpp
@@ -0,0 +1,28 @@
+/***********************************************************************
+* Program:
+*    Assignment 10, Hello World
+*    Brother Comeau, CS124
+* Author:
+*    Derek Anderson
+* Summary:
+*    The program will display a message. In this program, the text
+     "Hello World" will be displayed on the screen.
+*
+*    Estimated:  0.5 hrs
+*    Actual:     0.25 hrs
+*      The most difficult part for me was applying the endline to the
+       end of the message.
+************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+/**********************************************************************
+ * main will display the text "Hello World" on the screen.
+ ***********************************************************************/
+int main()
+{
+   std::cout << "Hello World\n";
+
+   return 0;
+}
